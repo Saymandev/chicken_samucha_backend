f3fd8840b412652a5d8ced55fdddef4be49c03ef
Saymandev <devrabbi255@gmail.com>
2025-09-16 19:15:54 +0600

    remove rate limit

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const xss = require('xss-clean');
-const rateLimit = require('express-rate-limit');
 const hpp = require('hpp');
 
 const connectDB = require('./config/database');
@@ -84,12 +83,7 @@ app.use(helmet({
 // Prevent XSS attacks
 app.use(xss());
 
-// Rate limiting
-const limiter = rateLimit({
-  windowMs: 10 * 60 * 1000, // 10 mins
-  max: 100
-});
-app.use(limiter);
+// Rate limiting disabled
 
 // Prevent http param pollution
 app.use(hpp());
