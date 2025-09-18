ad30301ce1a32b6e726bc151c25e08e5ee298f34
Saymandev <devrabbi255@gmail.com>
2025-09-18 11:19:41 +0600

    auth issue solve

--- routes/category.js
+++ routes/category.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const categoryController = require('../controllers/categoryController');
 const auth = require('../middleware/auth');
-const adminAuth = require('../middleware/adminAuth');
+const { authorize } = require('../middleware/auth');
 
 // Public routes
 router.get('/', categoryController.getAllCategories);
@@ -10,10 +10,10 @@ router.get('/navbar', categoryController.getNavbarCategories);
 router.get('/:id', categoryController.getCategoryById);
 
 // Admin routes (require authentication and admin role)
-router.post('/', auth, adminAuth, categoryController.createCategory);
-router.put('/:id', auth, adminAuth, categoryController.updateCategory);
-router.delete('/:id', auth, adminAuth, categoryController.deleteCategory);
-router.patch('/:id/toggle', auth, adminAuth, categoryController.toggleCategoryStatus);
-router.patch('/order', auth, adminAuth, categoryController.updateCategoryOrder);
+router.post('/', auth, authorize('admin'), categoryController.createCategory);
+router.put('/:id', auth, authorize('admin'), categoryController.updateCategory);
+router.delete('/:id', auth, authorize('admin'), categoryController.deleteCategory);
+router.patch('/:id/toggle', auth, authorize('admin'), categoryController.toggleCategoryStatus);
+router.patch('/order', auth, authorize('admin'), categoryController.updateCategoryOrder);
 
 module.exports = router;
