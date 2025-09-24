71c5596ba38ce21682c035445e1192b86f5cdf5f
Saymandev <devrabbi255@gmail.com>
2025-09-24 12:36:30 +0600

    navigation issue solve

--- routes/navigation.js
+++ routes/navigation.js
@@ -11,19 +11,19 @@ const {
   toggleNavigationMenuStatus
 } = require('../controllers/navigationController');
 const { validateNavigationMenu, validateReorder } = require('../middleware/navigationValidation');
-const adminAuth = require('../middleware/adminAuth');
+const { protect, authorize } = require('../middleware/auth');
 
 // Public routes
 router.get('/', getNavigationMenu);
 
 // Admin routes
-router.get('/admin', adminAuth, getAllNavigationMenus);
-router.get('/admin/:id', adminAuth, getNavigationMenuById);
-router.post('/admin', adminAuth, validateNavigationMenu, createNavigationMenu);
-router.put('/admin/:id', adminAuth, validateNavigationMenu, updateNavigationMenu);
-router.delete('/admin/:id', adminAuth, deleteNavigationMenu);
-router.post('/admin/reorder', adminAuth, validateReorder, reorderNavigationMenus);
-router.patch('/admin/:id/toggle', adminAuth, toggleNavigationMenuStatus);
+router.get('/admin', protect, authorize('admin'), getAllNavigationMenus);
+router.get('/admin/:id', protect, authorize('admin'), getNavigationMenuById);
+router.post('/admin', protect, authorize('admin'), validateNavigationMenu, createNavigationMenu);
+router.put('/admin/:id', protect, authorize('admin'), validateNavigationMenu, updateNavigationMenu);
+router.delete('/admin/:id', protect, authorize('admin'), deleteNavigationMenu);
+router.post('/admin/reorder', protect, authorize('admin'), validateReorder, reorderNavigationMenus);
+router.patch('/admin/:id/toggle', protect, authorize('admin'), toggleNavigationMenuStatus);
 
 module.exports = router;
 
